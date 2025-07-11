ab6995a4a672e5ec34987c42e22acc139d7b1577
pythontilk <noelburkhardt08@gmail.com>
2025-07-11 15:00:20 +0200

    fix: Remove duplicate Link import in Sidebar.tsx

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,25 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-
-import { DefaultSession, DefaultUser } from "next-auth";
-import { DefaultJWT } from "next-auth/jwt";
-
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      role: string;
-    } & DefaultSession["user"];
-  }
-
-  interface User extends DefaultUser {
-    role: string;
-  }
-}
-
-declare module "next-auth/jwt" {
-  interface JWT extends DefaultJWT {
-    role: string;
-  }
-}
